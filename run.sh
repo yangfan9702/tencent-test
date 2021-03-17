21ef6226693cbc1e8f56054dcf1e403a4799ec1b
yangfan <yangfan9702@outlook.com>
2021-03-17 11:16:01 +0800

    gunicorn

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python run.py & celery -A app.task:celery worker -l info & celery -A app.task:celery beat -s "celerybeat-async_task" --pidfile=
+gunicorn run:flask_app & celery -A app.task:celery worker -l info & celery -A app.task:celery beat -s "celerybeat-async_task" --pidfile=
