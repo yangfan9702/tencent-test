ef530617a4ebc19239041b7af9af620d8308593b
yangfan <yangfan9702@outlook.com>
2021-03-22 21:04:56 +0800

    network fix

--- app/setting.py
+++ app/setting.py
@@ -26,7 +26,7 @@ class ConfigProd:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../data'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    BROKER_URL = 'redis://127.0.0.1:6379/0'
+    BROKER_URL = 'redis://172.16.100.11:6379/0'
     CELERYBEAT_SCHEDULE = {
         'collect_course_data': {
             'task': 'app.task.timed_task.collect_course_data',
